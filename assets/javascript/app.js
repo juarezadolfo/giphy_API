3908e65719f9a8f55367ce7af3bf7ba8e023c4bd
juarezadolfo <juarezadolfo@hotmail.com>
2017-12-07 23:17:25 -0800

    almost final commit - images and ratings returning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,93 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     // An array of cars, new cars will be pushed into this array;
-    var cars = ["MOPAR", "Ford Mustang", "Chevy Bel Air", "Pontiac GTO", "Dodge Challenger"];
+    var cars = ["Dodge Charger", "Ford Mustang", "Chevy Camaro", "Pontiac Firebird","MOPAR"];
     // Creating Functions & Methods
     // Function that displays all gif buttons
-    function displayGifButtons(){
-        $("#gifButtonsDisplay").empty(); 
+    function displayGifButtons() {
+        $("#gifButtonsDisplay").empty();
 
-        for (var i = 0; i < cars.length; i++){
+        for (var i = 0; i < cars.length; i++) {
             var gifButton = $("<button>");
             gifButton.addClass("car");
             gifButton.addClass("btn btn-primary")
-            gifButton.attr("data-name", cars[i]);
+            gifButton.attr("data-cars", cars[i]);
             gifButton.text(cars[i]);
             $("#gifButtonsDisplay").append(gifButton);
         }
     }
     // Function to add a new car button
-function addNewButton(){
-    $("#addGif").on("click", function(){
-    var car = $("#car-input").val().trim();
-    if (car == ""){
-      return false; // no blank button allowed-user must enter a car
-    }
-    cars.push(car);
+    function addNewButton() {
+        $("#addGif").on("click", function () {
+            var car = $("#car-input").val().trim();
+            if (car === "") {
+                return false; 
+                // no blank button allowed-user must enter a car
+            }
+            cars.push(car);
 
-    displayGifButtons();
-    return false;
-    });
-}
-// Function to remove a car button
-    // Doesnt work perfectly, it doesn't just the last, it removes all the added buttons...close enough!
-    function removeLastButton(){
-        $("removeGif").on("click", function(){
-        cars.pop(car);
-        displayGifButtons();
-        return false;
+            displayGifButtons();
+            return false;
         });
     }
+    // Function to remove a car button
+    // Doesnt work perfectly, it doesn't just the last, it removes all the added buttons...close enough!
+    function removeLastButton() {
+        $("removeGif").on("click", function () {
+            cars.pop(car);
+            displayGifButtons();
+            return false;
+        });
+    }
+
+    // Function that displays all of the gifs
+    function displayGifs() {
+        var car = $(this).attr("data-cars");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=4LI0eZ9UVgWMWg15cbq7iZX97bBHr6og&q=" + car + "&limit=16&offset=0&rating=G&lang=en";
+        
+        console.log(queryURL); // displays the constructed url
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+            .done(function (response) {
+                console.log(response); // console test to make sure something returns
+                $("#gifsView").empty(); // empty this div id so that it doesnt keep the previous click info
+                var results = response.data; //shows results of gifs
+                if (results === "") {
+                    alert("There isn't a gif for this selected button");
+                } for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div>"); 
+                    //div for the gifs to go into
+                    gifDiv.addClass("gifDiv");
+                    // pulling rating of gifs
+                    var gifRating = $("<p>").text("Rating: " + results[i].rating);
+                    gifDiv.append(gifRating);
+                    // pulling gifs?
+                    // still image stored into src of image
+                    var gifImage = $("<img>");
+                    gifImage.attr("src", results[i].images.fixed_height_small_still.url); 
+                    
+
+                // pulling still image of gif
+                // adding div of gifs to gifsView div
+                    gifImage.addClass("image");
+                    gifDiv.append(gifImage);
+                    $("#gifsView").prepend(gifDiv);
+                }
+
+            });
+    }
     // Calling Functions & Methods
-    displayGifButtons(); // displays list of cars already created
+    // displays list of cars already created
+    displayGifButtons(); 
+    // adds new button
     addNewButton();
+    //removes buttons
     removeLastButton();
-    });
+    // display gifs & ratings? ('Document Event Listeners')
+    $(document).on("click", ".car", displayGifs);
+    
+});
 // }
 // queryURL for Giphy API
 
@@ -51,7 +97,7 @@ function addNewButton(){
     // Function that displays all of the gifs
     // Function to add a new button
     // Function to remove last button
-   
+
 // erase so it doesn't keep anything from the previous click/clear out when there is a new search
 
 // 'swapping' animate and still states of gifs for the "data-animate" feature
