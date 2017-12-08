af39aa2fc7b26baebcd14fa516a407504d68d72c
juarezadolfo <juarezadolfo@hotmail.com>
2017-12-07 16:59:40 -0800

    setup commit/first official commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+//array of dogs and/or cars, new dogs and/or cars will be pushed into this array
+
+// queryURL for Giphy API
+
+// retrieve gifs & buttons matching the value inputed by....
+// Creating Functions & Methods 
+    // Function that displays all gif buttons
+    // Function that displays all of the gifs
+    // Function to add a new button
+    // Function to remove last button
+   
+// erase so it doesn't keep anything from the previous click/clear out when there is a new search
+
+// 'swapping' animate and still states of gifs for the "data-animate" feature
+
